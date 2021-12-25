c1df9aeabdf487e7c8d77fdb433109fe5717019b
Christopher <chrisalexander0617@gmail.com>
2021-12-25 18:34:30 -0500

    added firebase function to component

--- src/routes/Find.jsx
+++ src/routes/Find.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import FindForm from '../components/FindForm'
+import { db } from '../firebase'
+import CardList from '../components/molecules/CardList'
+import Card from '../components/atoms/Card'
+
+import { collection, getDocs } from 'firebase/firestore'
 
 export default function Find(){ 
+    const [vehicles, setVehicles] = useState([])
+    const [query, setQuery] = useState("")
+    const vehiclesRef = collection(db, 'vehicles')
+   
+    useEffect(() => {
+        const getVehicles = async () => {
+        const data = await getDocs(vehiclesRef)
+        setVehicles(data.docs.map( doc => (
+                        {
+                            ...doc.data(), 
+                            id:doc.id 
+                        }
+                    )
+                )
+            )
+       }
+
+       getVehicles()
+    },[])
+
     return (
         <>
             <h1>Find Your Vehicle</h1>
-            <FindForm />
+            <form>
+                <input onChange type="text" placeholder="License"  />
+            </form>
+            <CardList>
+                {vehicles.map((vehicle, i) => {
+                    console.log(vehicle)
+                    return ( 
+                        <Card 
+                            key={i}
+                            make={vehicle.make}  
+                            model={vehicle.model} 
+                            color={vehicle.color}  
+                            license={vehicle.license} 
+                            location={vehicle.location} 
+                        />
+                    ) 
+                })}
+            </CardList>
         </>
     ) 
 }
